a063b91e1da4e9c6ea5520418ded04e2c234f122
alasiri <austinalasiri44@gmail.com>
2025-02-26 17:13:54 +0300

    Finished the loops

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(stringNames, eventNames){
+    let newArray = []
+    for(let i = 0; i < stringNames.length;i++){
+        newArray.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventNames } gift!`);
+       
+    }
+    return newArray;
+}
+
+function countDown (number){
+    let i = number;
+    while(i >= 0){
+        console.log(i)
+        i--;
+    }
+}
